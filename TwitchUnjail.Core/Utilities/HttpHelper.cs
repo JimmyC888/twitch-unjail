a49bf6433fc8b677c2f151a833eee3c64a433ad4
JimmyC888 <26488449+JimmyC888@users.noreply.github.com>
2025-10-05 20:35:27 -0400

    Add files via upload

--- TwitchUnjail.Core/Utilities/HttpHelper.cs
+++ TwitchUnjail.Core/Utilities/HttpHelper.cs
@@ -8,7 +8,8 @@ namespace TwitchUnjail.Core.Utilities {
     public static class HttpHelper {
 
         public const string DomainsUrl = "https://raw.githubusercontent.com/TwitchRecover/TwitchRecover/master/domains.txt";
-        public const string UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/108.0.5359.112 Mobile/15E148 Safari/604.1";
+
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36";
 
         public static async Task<bool> IsUrlReachable(string url) {
             try {
@@ -23,7 +24,11 @@ namespace TwitchUnjail.Core.Utilities {
             }
         }
 
-        public static async ValueTask<string[]> GetTwitchDomains() {
+        public static async ValueTask<string[]> GetTwitchDomains()
+        {
+            // Switched to a manual list for internal/private use.
+            // Previously fetched from: https://raw.githubusercontent.com/TwitchRecover/TwitchRecover/master/domains.txt
+            /*
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
             var response = await client.GetAsync(DomainsUrl);
@@ -32,8 +37,33 @@ namespace TwitchUnjail.Core.Utilities {
                 .Split('\n')
                 .Select(d => d.Trim())
                 .ToArray();
+            */
+
+            return new[]
+            {
+                "https://vod-secure.twitch.tv",
+                "https://vod-metro.twitch.tv",
+                "https://vod-pop-secure.twitch.tv",
+                "https://d2e2de1etea730.cloudfront.net",
+                "https://dqrpb9wgowsf5.cloudfront.net",
+                "https://ds0h3roq6wcgc.cloudfront.net",
+                "https://d2nvs31859zcd8.cloudfront.net",
+                "https://d2aba1wr3818hz.cloudfront.net",
+                "https://d3c27h4odz752x.cloudfront.net",
+                "https://dgeft87wbj63p.cloudfront.net",
+                "https://d1m7jfoe9zdc1j.cloudfront.net",
+                "https://d3vd9lfkzbru3h.cloudfront.net",
+                "https://d2vjef5jvl6bfs.cloudfront.net",
+                "https://d1ymi26ma8va5x.cloudfront.net",
+                "https://d1mhjrowxxagfy.cloudfront.net",
+                "https://ddacn6pr5v0tl.cloudfront.net",
+                "https://d3aqoihi2n8ty8.cloudfront.net",
+                "https://d3fi1amfgojobc.cloudfront.net"
+            };
+
         }
-        
+
+
         public static async ValueTask<string> GetHttp(string url) {
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
